433d97017af0ebca44a08d618361c51643b04b12
stein <astein@linnovate.net>
2014-03-13 16:26:42 +0200

    added style for the footer and imame slider in idea page

---
+++ scripts/socrates.js
@@ -0,0 +1,17 @@
+
+(function($) {
+
+	$(document).ready(function(){
+
+	  $('.pane-node-field-idea-image .field-items').bxSlider({
+			default: false,
+			pager: false,
+			slideMargin: 10,
+			minSlides: 1,
+			maxSlides: 3,
+			slideWidth: 190,
+		});
+	 
+	});     
+
+})(jQuery);   
