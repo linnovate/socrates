7217abb919f700e9e61fb80412ea6ef0d369232f
stein <astein@linnovate.net>
2014-03-17 17:02:12 +0200

    added style for comments

--- scripts/socrates.js
+++ scripts/socrates.js
@@ -3,15 +3,17 @@
 
 	$(document).ready(function(){
 
-	  $('.pane-node-field-idea-image .field-items').bxSlider({
-			default: false,
-			pager: false,
-			slideMargin: 10,
-			minSlides: 1,
-			maxSlides: 3,
-			slideWidth: 190,
-		});
-	 
+		if($('.pane-node-field-idea-image .field-items .field-item').length > 3) {
+			$('.pane-node-field-idea-image .field-items').bxSlider({
+				default: false,
+				pager: false,
+				slideMargin: 10,
+				minSlides: 1,
+				maxSlides: 3,
+				slideWidth: 190,
+			});
+		}
+
 	});     
 
 })(jQuery);   
