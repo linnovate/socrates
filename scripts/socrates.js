e7797bee72e0d7cff2152d364429ed9cbac36a1d
Marko Talijan <mtalijan@ecode.rs>
2014-03-28 12:12:14 +0100

    Admin menu behaviour, footer position, responsive layout

--- scripts/socrates.js
+++ scripts/socrates.js
@@ -17,34 +17,34 @@
 			//if user can see admin toolbar give normal menu margin on top depending if second row is horizontal
 			if($('.navbar-administration #navbar-item--2-tray').hasClass('navbar-active') && $('.navbar-administration #navbar-item--2-tray').hasClass('navbar-tray-horizontal'))
 				{
-					$('body.navbar-administration header#navbar').css('margin-top','78px');
+					$('body.navbar-administration header#navbar, .main-container').css('margin-top','78px');
 				}
 				else
 				{
-					$('body.navbar-administration header#navbar').css('margin-top','39px');
+					$('body.navbar-administration header#navbar, .main-container').css('margin-top','39px');
 				}
 			//or vertical
 			$('.navbar-administration a.navbar-icon').click(function(){
 				if($('#navbar-item--2-tray').hasClass('navbar-active') && $('.navbar-administration #navbar-item--2-tray').hasClass('navbar-tray-horizontal'))
 				{
-					$('body header#navbar').css('margin-top','39px');
+					$('body header#navbar, .main-container').css('margin-top','39px');
 				}
 				else
 				{
-					$('body header#navbar').css('margin-top','78px');
+					$('body header#navbar, .main-container').css('margin-top','78px');
 				}
 				if($('#navbar-item--2-tray').hasClass('navbar-tray-vertical'))
 				{
-					$('body header#navbar').css('margin-top','39px');
+					$('body header#navbar, .main-container').css('margin-top','39px');
 				}
 			});
 			//when user clicks on navbar toggle to toggle position horizontal or vertical
 			$('.navbar-administration .navbar-toggle').click(function(){
 				if($(this).val()=='horizontal'){
-					$('body header#navbar').css('margin-top','78px');
+					$('body header#navbar, .main-container').css('margin-top','78px');
 				}
 				else{
-					$('body header#navbar').css('margin-top','39px');	
+					$('body header#navbar, .main-container').css('margin-top','39px');	
 				}
 			});
 		
