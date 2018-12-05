951dd69c65d814be1ddaf1420c0833b6f96339fc
Konstantin Konyukhov <kvkonuhov@gmail.com>
2018-12-05 10:41:41 +0300

    update

--- config.rb
+++ config.rb
@@ -1,11 +1,11 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "css"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+http_path = "/" 
+css_dir = "css" 
+sass_dir = "sass" 
+images_dir = "images" 
+javascripts_dir = "javascripts" 
 
 # You can select your preferred output style
 # here (can be overridden via the command line):
@@ -20,6 +20,3 @@ javascripts_dir = "javascripts"
 # line_comments = false
 
 preferred_syntax = :sass
-
-Encoding.default_external = 'utf-8'
-encoding = 'utf-8'
