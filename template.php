92a77844d2ee495d4058cae7f65be1770d8cf839
Konstantin Konyukhov <kvkonuhov@gmail.com>
2018-12-06 15:37:48 +0300

    front page sidebar

--- template.php
+++ template.php
@@ -12,3 +12,13 @@ function socrates_menu_tree__main_menu($variables) {
 function socrates_menu_tree__user_menu($variables) {
   return '<ul class="menu nav navbar-nav">' . $variables['tree'] . '</ul>';
 }
+
+/**
+ * Implements hook_preprocess_HOOK().
+ */
+function socrates_preprocess_panels_pane(&$variables) {
+  if (!empty($variables['pane']->configuration['title_class'])) {
+    $class = explode(' ', $variables['pane']->configuration['title_class']);
+    $variables['title_attributes_array']['class'] = array_merge($variables['title_attributes_array']['class'], $class);
+  }
+}
